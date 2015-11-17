a48ac34a85cc154131c19978ed3af0aeb0923553
Ooya <thibaut.castanie@gmail.com>
2015-11-17 23:44:38 +0100

    Ajout caméra supérieure suivant le joueur

---
+++ Urban Wheelchair/Assets/scripts/camFollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camFollowPlayer : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		transform.position = new Vector3(player.transform.position.x,10,player.transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(player.transform.position.x,10,player.transform.position.z);
+	}
+}
---
+++ Urban Wheelchair/Assets/scripts/changeCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class changeCamera : MonoBehaviour {
+	
+	public Camera cameraFauteuil;
+	public Camera cameraPlan;
+
+	// Use this for initialization
+	void Start () {
+		cameraFauteuil.enabled = true;
+		cameraPlan.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.C)) {
+			cameraFauteuil.enabled = !cameraFauteuil.enabled;
+			cameraPlan.enabled = !cameraPlan.enabled;
+		}
+	}
+}
